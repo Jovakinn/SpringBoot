d38b5fef70723ed1bf5206c41cbc76204e44d257
Jovakinn <max05012004@gmail.com>
2022-02-20 17:04:45 +0200

    add logging

--- fraud/src/main/java/com/example/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/example/fraud/controller/FraudController.java
@@ -3,11 +3,13 @@ package com.example.fraud.controller;
 import com.example.fraud.models.FraudCheckResponse;
 import com.example.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -15,6 +17,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         Boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
