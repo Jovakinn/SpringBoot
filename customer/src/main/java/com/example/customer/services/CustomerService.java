ab7ab68030dfe0253ec9a7e06b31bd2581f29aef
Jovakinn <max05012004@gmail.com>
2022-02-20 02:09:25 +0200

    add additional check

--- customer/src/main/java/com/example/customer/services/CustomerService.java
+++ customer/src/main/java/com/example/customer/services/CustomerService.java
@@ -3,14 +3,17 @@ package com.example.customer.services;
 import com.example.customer.models.Customer;
 import com.example.customer.models.CustomerRegistrationRequest;
 import com.example.customer.repository.CustomerRepository;
+import com.example.fraud.models.FraudCheckResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -19,6 +22,14 @@ public class CustomerService {
                 .email(customerRequest.email())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
